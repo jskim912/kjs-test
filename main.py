53786d71be4af1abe39d08fa0aa4967824f16daf
jskim912 <jskim@storelink.io>
2023-07-03 17:41:36 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+def main(event, context):
+    print("This is Multi-Cloud Deployment Test.")
+    print("-"*30)
+
+    resp = requests.get("https://www.google.co.kr/?hl=ko")
+    print("resp: ", resp)
+
+
+if __name__ == "__main__":
+    main()
