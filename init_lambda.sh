14573cc3293d087a93a63e3011eb14579a8f89ef
jskim912 <jskim@storelink.io>
2023-07-05 14:44:15 +0900

    modify

--- init_lambda.sh
+++ init_lambda.sh
@@ -3,7 +3,7 @@ cat << EOF > main.py
 from application import main
 
 def start(event, context):
-   """Lambda 진입 함수
+    """Lambda 진입 함수
 
     Args:
         event (dict): Lambda로 들어온 Request 객체 데이터
@@ -12,6 +12,7 @@ def start(event, context):
     Returns:
         json: 파싱된 결과 데이터
     """
+
     result = main(event)
     return result
 EOF
