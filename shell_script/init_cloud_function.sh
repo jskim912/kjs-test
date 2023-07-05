79c0bf08f7f7692846940632b0fdbac9c11bf015
jskim912 <jskim@storelink.io>
2023-07-05 18:07:03 +0900

    modify

--- shell_script/init_cloud_function.sh
+++ shell_script/init_cloud_function.sh
@@ -13,5 +13,5 @@ mkdir package
 mv $(ls | grep -v -e package) package
 
 # deployment
-/Users/jskim/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/Users/jskim/gcp-363375785641-compute-key.json
+/Users/jskim/google-cloud-sdk/bin/gcloud auth activate-service-account 363375785641-compute@developer.gserviceaccount.com --key-file=/Users/jskim/gcp-363375785641-compute-key.json
 /Users/jskim/google-cloud-sdk/bin/gcloud functions deploy $1 --trigger-http --runtime=python310 --region=${2:-asia-northeast3} --source=package
