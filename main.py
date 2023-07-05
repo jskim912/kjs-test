3d8aa2b0ddbb27cff28f272119be2038a4f0a544
jskim912 <jskim@storelink.io>
2023-07-05 10:26:07 +0900

    '-' to '#'

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 def main(event, context):
     print("This is Multi-Cloud Deployment Test.")
-    print("-"*30)
+    print("#"*30)
 
     resp = requests.get("https://www.google.co.kr/?hl=ko")
     print("resp: ", resp)
