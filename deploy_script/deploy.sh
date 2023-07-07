a1718f92353fde0840367bec134d4ddc2f3668ac
jskim912 <jskim@storelink.io>
2023-07-07 15:50:57 +0900

    modify

--- deploy_script/deploy.sh
+++ deploy_script/deploy.sh
@@ -1,62 +1,72 @@
 #!/bin/bash
 if [ $# -lt 1 ]; then
-    echo "Usage: deploy.sh <cloud_platform> <function_name_prefix> <region>"
-    echo " ex1) deploy.sh AWS COUPANG_REVIEW [ap-northeast-2, ap-southeast-1]"
-    echo " ex2) deploy.sh GCP COUPANG_REVIEW [asia-northeast1, asia-northeast2, asia-northeast3]"
+    echo "Usage: deploy.sh"
     exit 1
 fi
 
-CLOUD=${1:-AWS}
-FUNCTION_NAME_PREFIX=$2
-AWS_REGION=${3:-ap-northeast-2}
-GCP_REGION=${3:-asia-northeast3}
-DEPLOY_NUM=1
+CLOUD_LIST=(AWS GCP)
+AWS_REGION_LIST=(ap-northeast-1 ap-southeast-2)
+GCP_REGION_LIST=(asia-northeast1 asia-northeast2 asia-northeast3)
+AWS_REGION=${2:-ap-northeast-2}
+GCP_REGION=${2:-asia-northeast3}
 
-if [ $CLOUD == "AWS" ] 
-then
-    
-    # build
-    cp entry_point/main_lambda.py .
 
-    # install dependencies
-    python3 -m pip install --upgrade pip
-    pip3 install virtualenv
+for CLOUD in $CLOUD_LIST
+do
+    #####################################################################
+    # AWS Lambda
+    #####################################################################
+    if [ $CLOUD == "AWS" ] 
+    then
+        for REGION in $AWS_REGION_LIST
+        do
+            # build
+            cp entry_point/main_lambda.py .
 
-    /opt/homebrew/bin/virtualenv venv -p /opt/homebrew/bin/python3.10
-    #python3 -m venv venv
-    source ./venv/bin/activate
-    
-    pip3 install -r requirements.txt
+            # install dependencies
+            python3 -m pip install --upgrade pip
+            pip3 install virtualenv
 
-    # packaging
-    cd ./venv/lib/python3.10/site-packages
-    zip -r ../../../../package.zip .
-    cd ../../../../
-    zip -r package.zip application.py
-    zip -r package.zip main_lambda.py
+            # PATH 설정 필요
+            /opt/homebrew/bin/virtualenv venv -p /opt/homebrew/bin/python3.10 
+            source ./venv/bin/activate
 
-    # deployment
-    # /usr/local/bin/aws lambda update-function-code --function-name $1 --zip-file fileb://package.zip --region ${2:-ap-northeast-2}
-    for ((i=1; i<$DEPLOY_NUM+1; i++ ));
-    do
-        str_num="00${i}"
-        num="${str_num:(-3)}"
-        /usr/local/bin/aws lambda create-function --function-name ${FUNCTION_NAME_PREFIX}_${num} --runtime python3.10 --role arn:aws:iam::686449765408:role/storelink --handler main_lambda.entry --region $AWS_REGION --zip-file fileb://package.zip
-    done
+            pip3 install -r requirements.txt
 
-elif [ $CLOUD == "GCP" ]
-then
+            # packaging
+            cd ./venv/lib/python3.10/site-packages
+            zip -r ../../../../package.zip .
+            cd ../../../../
+            zip -r package.zip application.py
+            zip -r package.zip main_lambda.py
 
-    # build
-    cp entry_point/main.py .
+            # deployment
+            # 서비스 역할 정책 필요
+            # 함수 네이밍은 뭐가 좋을지
+            /usr/local/bin/aws lambda create-function --function-name test_${REGION} --runtime python3.10 --role arn:aws:iam::686449765408:role/storelink --handler main_lambda.entry --region $REGION --zip-file fileb://package.zip
+        done
 
-    # packaging
-    rm -rf package
-    mkdir package
-    mv $(ls | grep -v -e package) package
 
-    # deployment
-    #/Users/jskim/google-cloud-sdk/bin/gcloud functions deploy $1 --trigger-http --runtime=python310 --region=$GCP_REGION --source=package
-    /Users/jskim/google-cloud-sdk/bin/gcloud auth activate-service-account 363375785641-compute@developer.gserviceaccount.com --key-file="/Users/jskim/gcp-363375785641-compute-key.json"
-    /Users/jskim/google-cloud-sdk/bin/gcloud functions deploy ${FUNCTION_NAME_PREFIX}_001 --trigger-http --runtime=python310 --region=$GCP_REGION --source=package --entry-point=entry
-fi
+    #####################################################################
+    # Google Cloud Function
+    #####################################################################
+    elif [ $CLOUD == "GCP" ]
+    then
+
+        for REGION in $AWS_REGION_LIST
+        do
+            # build
+            cp entry_point/main.py .
+
+            # packaging
+            rm -rf package
+            mkdir package
+            mv $(ls | grep -v -e package) package
+
+            # deployment
+            /Users/jskim/google-cloud-sdk/bin/gcloud auth activate-service-account 363375785641-compute@developer.gserviceaccount.com --key-file="/Users/jskim/gcp-363375785641-compute-key.json"
+            /Users/jskim/google-cloud-sdk/bin/gcloud functions deploy test_${REGION} --trigger-http --runtime=python310 --region=$REGION --source=package --entry-point=entry
+        done
+
+    fi
+done
