4d3a3ef615ac2f5d2e2284e16dbd832628a65fb1
jskim912 <jskim@storelink.io>
2023-07-14 16:02:29 +0900

    modify

--- deploy_script/deploy_dev.sh
+++ deploy_script/deploy_dev.sh
@@ -44,7 +44,7 @@ function aws_lambda_deploy() {
         # echo "existFunc : $existFunc"
         
         updateFunc=$(/usr/local/bin/aws lambda update-function-code --function-name ${FUNC_NAME}-${REGION} --region $REGION --zip-file fileb://package.zip)
-        if [ $updateFunc == '' ]
+        if [ $updateFunc == "" ]
         then 
             createFunc=$(/usr/local/bin/aws lambda create-function --function-name ${FUNC_NAME}-${REGION} --runtime python3.10 --role arn:aws:iam::686449765408:role/storelink --handler main.entry --region $REGION --zip-file fileb://package.zip)
             echo "createFunc : $createFunc"
