78a94da22f5bc725a2db08302aa3f0cee91ce6f4
jskim912 <jskim@storelink.io>
2023-07-14 17:27:44 +0900

    modify

--- deploy_script/deploy_dev.sh
+++ deploy_script/deploy_dev.sh
@@ -65,7 +65,7 @@ function gcp_cloud_function_deploy() {
     echo $(ls | grep -v package)
     ls -al
     
-    mv $(echo * | sed s:package::g) package
+    mv -v $(echo * | sed s:package::g) package
     ls -al
     
 
